f6059e45cee7e8f1f80bc3bcfc13dd12250ed970
Xuming <voicevon@gmail.com>
2021-09-26 12:30:48 +0800

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'VonPylib',
+    version = '0.0.1',
+    description = 'My own python liberary',
+    py_modules = ["terminal_font",
+                    "reprap_arm"],
+    package_dir = {'': 'src'},
+    # Optional
+    # author-email = 'voicevon@gmail.com',
+
+)
